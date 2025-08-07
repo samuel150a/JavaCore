99f28f592de7b4621368af99dd41e40aadf4aafb
samuel150a <samuelofficial184@gmail.com>
2025-08-07 23:58:42 +0530

    File Saving

--- Interior-works/src/main/java/com/xworkz/interiorwork/service/InteriorService.java
+++ Interior-works/src/main/java/com/xworkz/interiorwork/service/InteriorService.java
@@ -3,6 +3,7 @@ package com.xworkz.interiorwork.service;
 import com.xworkz.interiorwork.entity.InteriorEntity;
 
 import java.util.List;
+import java.util.Map;
 
 public interface InteriorService {
     InteriorEntity validation(InteriorEntity en);
@@ -23,6 +24,25 @@ public interface InteriorService {
     List<InteriorEntity> getBYSpecificvariety(String entity);
     public InteriorEntity getInteriorEntityByPrice(Integer price);
     public InteriorEntity getInteriorEntityBydoordelivery(String doordelivery);
+    public InteriorEntity findByNameByTypedQuery(String n);
+    public InteriorEntity updateById(String updatedtype,String updatedShop,Integer Id);
+    InteriorEntity updateByPrice(String updatedVariety,Integer Price);
+    InteriorEntity updateByShop(Boolean updateWood,String updateShop,Integer Id);
+    List<String> getAllVarietynames();
+    List<Integer>getAllPrice();
+    List<Object>getAllWood();
+    List<Object[]> FetchTwoColumns();
+
+
+    //6/08/2025
+
+    List<Object[]> FetchThreeColumns();
+
+
+
+
+
+
 
 
 }
